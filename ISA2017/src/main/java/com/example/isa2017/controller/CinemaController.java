fa528ca16cd14433aaea14e4ccf930558250211a
Igor Zeljkovic <zeljkovic18@gmail.com>
2018-04-04 01:43:02 +0200

    zapocet cinema kontroler.

---
+++ ISA2017/src/main/java/com/example/isa2017/controller/CinemaController.java
@@ -0,0 +1,10 @@
+package com.example.isa2017.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "cinemas")
+public class CinemaController {
+
+}
