e097a20d01b97c7c73c82a8241653c9960e9614b
Ivana <anticivana27@gmail.com>
2018-04-05 20:21:21 +0200

    Kreuran model user

--- ISA2017/src/main/java/com/example/isa2017/model/User.java
+++ ISA2017/src/main/java/com/example/isa2017/model/User.java
@@ -10,114 +10,88 @@ import javax.persistence.Id;
 
 
 
-@Entity
+@Entity(name="user")
 public class User {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name="email")
 	private String email;
-	private String name;
-	private String surname;
+	
+	@Column(name="password")
 	private String password;
 	
-	@Column(name = "role", nullable = false)
-    @Enumerated(EnumType.STRING)
-    private Role role;
+	@Column(name="name")
+	private String name;
 	
-	private boolean isEnabled=false;
+	@Column(name="surname")
+	private String surname;
+	
+	@Column(name="phoneNumber")
+	private String phoneNumber;
+	
+	@Column(name="city")
 	private String city;
-	private String phone;
 	
-	public User(){
-		
-	}
-
-	public User(Long id, String email, String name, String surname, String password, Role role, boolean isEnabled,
-			String city, String phone) {
+	
+	
+	public User(){}
+	
+	
+	public User(String email, String password, String name, String surname, String phoneNumber, String city) {
 		super();
-		this.id = id;
 		this.email = email;
+		this.password = password;
 		this.name = name;
 		this.surname = surname;
-		this.password = password;
-		this.role = role;
-		this.isEnabled = isEnabled;
+		this.phoneNumber = phoneNumber;
 		this.city = city;
-		this.phone = phone;
 	}
-
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getSurname() {
-		return surname;
-	}
-
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-	public Role getRole() {
-		return role;
+	public String getName() {
+		return name;
 	}
-
-	public void setRole(Role role) {
-		this.role = role;
+	public void setName(String name) {
+		this.name = name;
 	}
-
-	public boolean isEnabled() {
-		return isEnabled;
+	public String getSurname() {
+		return surname;
 	}
-
-	public void setEnabled(boolean isEnabled) {
-		this.isEnabled = isEnabled;
+	public void setSurname(String surname) {
+		this.surname = surname;
 	}
-
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 	public String getCity() {
 		return city;
 	}
-
 	public void setCity(String city) {
 		this.city = city;
 	}
 
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
 
 	
 
