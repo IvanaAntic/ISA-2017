437862a96171305b24a5830d13925ebfc9e59c3e
Ivana <anticivana27@gmail.com>
2018-04-06 01:46:45 +0200

    Dodata klasa UserDTO

---
+++ ISA2017/src/main/java/com/example/isa2017/modelDTO/UserDTO.java
@@ -0,0 +1,94 @@
+package com.example.isa2017.modelDTO;
+
+import com.example.isa2017.model.Role;
+
+public class UserDTO {
+
+	private Long id;
+	private String email;
+	private String name;
+	private String surname;
+	private String password;
+	private String city;
+	private String phone;
+	private Role role;
+	
+	public UserDTO(){}
+	
+	public UserDTO(String email, String name, String surname, String password, String city, String phone, Role role) {
+		super();
+		this.email = email;
+		this.name = name;
+		this.surname = surname;
+		this.password = password;
+		this.city = city;
+		this.phone = phone;
+		this.role = role;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	
+	
+}
---
+++ ISA2017/src/main/java/com/example/isa2017/service/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.example.isa2017.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.isa2017.model.User;
+import com.example.isa2017.modelDTO.UserDTO;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+	@Override
+	public User save(UserDTO user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
