491e478729dfa478d049c529aedc9ec46eb4aeab
Ivana <anticivana27@gmail.com>
2018-04-05 21:50:08 +0200

    Napravljen deo funkcionalnosti za registraciju

--- ISA2017/src/main/java/com/example/isa2017/controller/UserController.java
+++ ISA2017/src/main/java/com/example/isa2017/controller/UserController.java
@@ -1,5 +1,30 @@
 package com.example.isa2017.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.isa2017.model.User;
+import com.example.isa2017.modelDTO.UserDTO;
+import com.example.isa2017.service.UserService;
+
+@RestController
+@RequestMapping(value="/user")
 public class UserController {
 
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(value="/register" , method=RequestMethod.POST)
+	public ResponseEntity<User> registerUser(@RequestBody UserDTO user){
+		User user1 = userService.save(user);
+		return new ResponseEntity<> (user1,HttpStatus.OK);
+	}
+	
+	
+	
 }
--- ISA2017/src/main/java/com/example/isa2017/service/UserService.java
+++ ISA2017/src/main/java/com/example/isa2017/service/UserService.java
@@ -2,7 +2,13 @@ package com.example.isa2017.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.isa2017.model.User;
+import com.example.isa2017.modelDTO.UserDTO;
+
+
 @Service
 public interface UserService {
+	
 
+	User save(UserDTO user);
 }
