34534762355c9650c002830ba871a5b78e4fb958
Ivana <anticivana27@gmail.com>
2018-04-14 23:05:29 +0200

    Dodate metode u UserService

--- ISA2017/src/main/java/com/example/isa2017/service/UserServiceImpl.java
+++ ISA2017/src/main/java/com/example/isa2017/service/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.isa2017.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.mail.MailException;
@@ -20,6 +22,7 @@ public class UserServiceImpl implements UserService{
 	private JavaMailSender javaMailSender;
 	@Autowired
 	private Environment  env;
+	
 	@Override
 	public User save(UserDTO user)  {
 		// TODO Auto-generated method stub
@@ -112,6 +115,21 @@ public class UserServiceImpl implements UserService{
 		userRepository.save(user1);
 		
 		return user1;
+	}
+	@Override
+	public List<User> findAll() {
+		// TODO Auto-generated method stub
+		return userRepository.findAll();
+	}
+	@Override
+	public User findByEmail(String email) {
+		// TODO Auto-generated method stub
+		return userRepository.findByEmail(email);
+	}
+	@Override
+	public User findById(Long id) {
+		// TODO Auto-generated method stub
+		return userRepository.findById(id);
 	}
 	
 
